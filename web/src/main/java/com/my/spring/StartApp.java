18c33f4b99b7eb2f46520a99b9c77c21d60ffb94
syxiao <syxiao@thoughtworks.com>
2018-08-15 18:07:41 +0800

    structure

---
+++ web/src/main/java/com/my/spring/StartApp.java
@@ -0,0 +1,20 @@
+package com.my.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class StartApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApp.class, args);
+    }
+
+    @GetMapping("/")
+    public String hello() {
+        return "Hello, world!";
+    }
+}
