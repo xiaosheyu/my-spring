5d51f5de730d1f80ca5f7240a62f9fd2bd59a007
syxiao <syxiao@thoughtworks.com>
2018-09-03 15:11:12 +0800

    log

--- web/src/main/java/com/my/spring/StartApp.java
+++ web/src/main/java/com/my/spring/StartApp.java
@@ -2,7 +2,6 @@ package com.my.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,10 +10,5 @@ public class StartApp {
 
     public static void main(String[] args) {
         SpringApplication.run(StartApp.class, args);
-    }
-
-    @GetMapping("/")
-    public String hello() {
-        return "Hello, world!";
     }
 }
